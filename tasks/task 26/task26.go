7a329d40effb1232f7a5e38fa475046f92f08354
tr0llex <trollex2013@gmail.com>
2021-07-09 18:21:51 +0300

    Reduced the number of iterations in the loop

--- tasks/task 26/task26.go
+++ tasks/task 26/task26.go
@@ -5,8 +5,11 @@ import "fmt"
 func reverse(a string) string {
 	slice := []rune(a)
 	var temp []rune
-	for i := len(slice) - 1; i >= 0; i-- {
-		temp = append(temp, slice[i])
+	for i := len(slice) - 1; i >= 1; i -= 2 {
+		temp = append(temp, slice[i], slice[i-1])
+	}
+	if len(slice)%2 == 1 {
+		temp = append(temp, slice[0])
 	}
 	return string(temp)
 }
